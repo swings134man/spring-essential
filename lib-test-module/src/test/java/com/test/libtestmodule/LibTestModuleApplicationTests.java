238cbb5b9a21b4ad7a73a894c90763e9ea812c29
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-12-18 18:47:08 +0900

    ADD: Spring boot Library TEST Module INIT

---
+++ lib-test-module/src/test/java/com/test/libtestmodule/LibTestModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.libtestmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibTestModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

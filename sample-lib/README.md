81144e65b5098e064eb869b38b9cb171b5e8ee6f
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-12-18 18:42:06 +0900

    ADD: Spring boot Library Sample Module INIT

---
+++ sample-lib/README.md
@@ -0,0 +1,5 @@
+# Spring boot Library Sample Module
+
+- For Spring Boot Library Project Modules
+- This is a sample module
+> Request -> Log output -> Response
---
+++ sample-lib/src/main/java/com/lucas/samplelib/config/SampleAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.lucas.samplelib.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.lucas.samplelib")
+public class SampleAutoConfiguration {
+}

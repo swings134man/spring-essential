933794d2bd9f5010e6d8a34daeeb03aaf53f5006
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2025-06-10 01:24:01 +0900

    chore: MockK

--- kopring-jpa-demo/build.gradle.kts
+++ kopring-jpa-demo/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+    testImplementation("io.mockk:mockk:1.13.8") // MockK
 }
 
 kotlin {
